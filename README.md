b7c1bc265d1732c6ece41911212468d9c9e10d45
João Pedro Barros <joao@joaopedrobarros.com.br>
2019-02-09 17:37:53 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdonisJs Scout
+
+Adonis Scout provides a driver based solution for searching your Lucid models, just like Laravel Scout.
---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @brainnit/adonisjs-scout
+ *
+ * (c) João Pedro Barros <joao@joaopedrobarros.com.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+'use strict'
+
+module.exports = {}
