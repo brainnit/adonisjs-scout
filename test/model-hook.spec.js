312f0c7c1807bcb16b0a9f57df65ccc8eac03221
João Pedro Barros <joao@joaopedrobarros.com.br>
2019-02-12 14:22:04 -0200

    ModelHook tests

---
+++ test/model-hook.spec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const ModelHook = require('../src/ModelHook')
+
+describe('ModelHook', () => {
+  it('makes model searchable when saved if should be searchable', () => {
+    const modelMock = jest.fn()
+    modelMock.shouldBeSearchable = jest.fn(() => true)
+    modelMock.searchable = jest.fn()
+    ModelHook.saved(modelMock)
+    expect(modelMock.searchable).toHaveBeenCalledTimes(1)
+  })
+
+  it('makes model unsearchable when saved if should NOT be searchable', () => {
+    const modelMock = jest.fn()
+    modelMock.shouldBeSearchable = jest.fn(() => false)
+    modelMock.unsearchable = jest.fn()
+    ModelHook.saved(modelMock)
+    expect(modelMock.unsearchable).toHaveBeenCalledTimes(1)
+  })
+
+  it('makes model unsearchable when deleted', () => {
+    const modelMock = jest.fn()
+    modelMock.unsearchable = jest.fn()
+    ModelHook.deleted(modelMock)
+    expect(modelMock.unsearchable).toHaveBeenCalledTimes(1)
+  })
+})
