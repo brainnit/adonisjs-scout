8618c6b687ef4cd6ba9a73e7e6b59a75eb930829
João Pedro Barros <joao@joaopedrobarros.com.br>
2019-02-12 16:18:39 -0200

    Fix EngineManager tests

--- test/engine-manager.spec.js
+++ test/engine-manager.spec.js
@@ -3,7 +3,7 @@
 const EngineManager = require('../src/Manager')
 const NullDriver = require('../src/Drivers').null
 
-describe('Engine Manager', () => {
+describe('EngineManager', () => {
   it('throw exception when invalid driver name is passed', () => {
     const fn = () => (new EngineManager()).driver('foo', {})
     expect(fn).toThrow('E_INVALID_DRIVER: foo is not a valid search engine driver')
@@ -17,5 +17,12 @@ describe('Engine Manager', () => {
   it('get instance of existing driver', () => {
     const driver = (new EngineManager()).driver('null', {})
     expect(driver).toBeInstanceOf(NullDriver)
+  })
+
+  it('extend adds driver to known drivers list', () => {
+    const manager = new EngineManager()
+    const driverMock = jest.fn()
+    manager.extend('drivermock', driverMock)
+    expect(manager._drivers['drivermock']).toEqual(driverMock)
   })
 })
